cb936680945231dd544b3e3ebb60453e47422b29
Vijay <rebalvijay930@gmail.com>
2025-02-06 13:53:26 +0530

    first change

---
+++ Variables/variables.py
@@ -0,0 +1,25 @@
+print("Hello world")
+x=input("Enter name")
+y=int(input("Enter number"))
+print(x)
+print(y)
+
+# Output in the world of python
+print("I am Vishwa")
+
+# Support for the formatted string
+name = "Vishwa"
+age = 99
+print("Hello, my name is ", name, "and my age is ", age)
+
+print(f"My name is {name} and age is {age}")
+
+x=2
+y=3
+z=4
+print(x,y,z,sep=',')
+
+print("Hello",end=" ")
+print("World",end=" ")
+print("!")
+ 
---
+++ datatypes/datatypes.py
@@ -0,0 +1,29 @@
+# numerical
+x = 5
+
+price = 24.25
+print(price)
+
+num = 3e6
+# e=3*10^6
+print(num)
+
+z = 4 + 5
+print(z)
+
+# type
+print(type(z))
+
+print(abs(-5))
+print(pow(2,3))
+
+print(round(4.75))
+print(round(4.5))
+
+nums = [3,1,5,7,9,1]
+
+print(min(nums))
+print(max(nums))
+
+print(sum(nums))
+
